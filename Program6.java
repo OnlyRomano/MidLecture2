f7f0938b32e1b17aa99a5500f3d6d565112579b2
Jhun Mark <jhunmarkromano2@gmial.com>
2023-09-19 17:32:15 +0800

    multi-Level inheritance

--- Program6.java
+++ Program6.java
@@ -15,6 +15,25 @@ public class Program6 {
 
                 choice = sc.nextInt();
             } while (choice < 1 || choice > 3);
+
+            Average average = new Average();
+
+            if (choice == 1) {
+                System.out.print("Input Number: ");
+                average.setdata1(sc.nextInt());
+                System.out.print("Input Number: ");
+                average.setdata2(sc.nextInt());
+
+                average.Add();
+                average.Subtract();
+                average.Divide();
+                average.Multiply();
+
+                System.out.println("Sum: " + average.getSum());
+                System.out.println("Difference: " + average.getDifferent());
+                System.out.println("Product: " + average.getProduct());
+                System.out.println("Quotient: " + average.getQuotient());
+            }
         }
 
         catch (InputMismatchException error) {
